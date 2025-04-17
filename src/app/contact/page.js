c605644fc9e58142dc5cea518d94c3cfc2b63520
Hadi Khan <hadi70037@gmail.com>
2025-04-17 15:17:14 -0400

    Update page.js

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,10 +1,10 @@
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function Contact(){
     return (
         <>
-            <NavBar />
+            <Navbar />
             <main style={{ padding: '1rem'}}>
                 <h1>Contact Us</h1>
                 <p>Email: support.eventsapp@gmail.com</p>
