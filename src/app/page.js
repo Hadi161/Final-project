6487de18f9361953dca6c32c9537261fa66a3c24
Hadi Khan <hadi70037@gmail.com>
2025-04-17 14:11:23 -0400

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,27 @@
-'use client';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useState } from 'react';
+import NavBar from '/components/NavBar';
+import Footer from '/components/Footer';
+import EventForm from '/components/EventForm';
+import EventList from '/components/EventList';
 
-export default function HomePage() {
-  const router = useRouter();
+export default function Home() {
+  const [events, setEvents] = useState ([]);
 
-  return (
-    <div>
-      <h1>Welcome to the Event App</h1>
-      <p>Discover upcoming events and connect with us.</p>
-      <button onClick={() => router.push('/events')}>
-        View Events
-      </button>
-    </div>
+  const addEvent = (event) => {
+    setEvents([...events, event]);
+  };
+
+
+  return(
+    <>
+      <NavBar />
+      <main style={{ padding: '1rem'}}>
+        <h1>Event Manager</h1>
+        <EventForm onAdd={addEvent} />
+        <EventList events={events} />
+      </main>
+      <Footer />
+    </>
   );
 }
